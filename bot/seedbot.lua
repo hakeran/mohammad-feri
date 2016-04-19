a99a5ae139bcb1e0eb47d0820ea6844127714b66
hakeran <mohammadmehdirazi1381@gmail.com>
2016-04-19 09:30:58 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {218045228},--Sudo users
+    sudo_users = {218045228,87832986},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
